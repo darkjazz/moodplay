e08ab3dc3d60277933ce3c5da88c039a68972506
alo <alo@anrita.local>
2018-07-12 15:39:57 +0100

    added shared files

---
+++ src/app/shared/config.ts
@@ -0,0 +1,5 @@
+export const Config = {
+  // server: `https://moodplay-data.herokuapp.com`,
+  server: "http://127.0.0.1:6666",
+  moodplay: "/moodplay"
+}
---
+++ src/app/shared/models.ts
@@ -0,0 +1,47 @@
+export class Artist{
+  name: string;
+  mbid: string;
+  deezer_id: Number;
+}
+
+export class Coords{
+  valence: Number;
+  arousal: Number;
+  dominance: Number;
+}
+
+export class Track {
+  id: Number;
+  ilm_id: string;
+  mbid: string;
+  deezer_id: Number;
+  filename: string;
+  song_title: string;
+  track_duration: string;
+  album_title: string;
+  release_year: string;
+  artist: Artist;
+  coords: Coords;
+}
+
+export class TrackCoords{
+  valence: Number;
+  arousal: Number;
+  filename: string;
+  artist: string;
+  title: string;
+}
+
+export class ArtistCoords{
+  name: string;
+  valence: Number;
+  arousal: Number;
+  track_count: Number;
+}
+
+export class Limits{
+  max_valence: Number;
+  min_valence: Number;
+  max_arousal: Number;
+  min_arousal: Number;
+}
