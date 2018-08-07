3e7bdb9d52b5202cc36a51c4c6b79c279044837c
florianthalmann <florianthalmann@bluewin.ch>
2018-08-07 17:57:22 +0900

    added transitions to tracks and moods

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { DeezerService } from '../services/deezer.service';
+import { MoodplayService } from '../services/moodplay.service';
 import { FeatureExtractionService } from '../services/feature-extraction.service';
-import { ArtistCoords } from '../shared/models';
+import { ArtistCoords, TrackCoords, Mood } from '../shared/models';
 import { AutoDj } from '../mix/auto-dj';
 
 @Injectable()
@@ -9,14 +10,25 @@ export class PlayerService  {
 
   private dj: AutoDj;
 
-  constructor(private deezer: DeezerService, features: FeatureExtractionService) {
+  constructor(private deezer: DeezerService, features: FeatureExtractionService,
+    private moodplayService: MoodplayService) {
     this.dj = new AutoDj(null, features);
     this.dj.init();
   }
 
   async transitionToArtist(artist: ArtistCoords) {
-    const songClip = await this.deezer.get30SecClip(artist.name);
-    this.dj.transitionToSong(songClip);
+    const clip = await this.deezer.get30SecClip(artist.name);
+    this.dj.transitionToSong(clip);
+  }
+
+  async transitionToTrack(track: TrackCoords) {
+    const clip = await this.deezer.get30SecClip(track.filename);
+    this.dj.transitionToSong(clip);
+  }
+
+  async transitionToMood(mood: Mood) {
+    const track = await this.moodplayService.getNearestTrack(mood.valence, mood.arousal);
+    this.dj.transitionToSong(track.filename);
   }
 
 }
